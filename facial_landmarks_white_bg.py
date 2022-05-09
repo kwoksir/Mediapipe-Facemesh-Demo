42e58be79240a169da5fe770b684b0ef1914f948
Dominic Kwok <61585411+kwoksir@users.noreply.github.com>
2022-05-09 10:17:37 +0800

    Update facial_landmarks_white_bg.py

--- facial_landmarks_white_bg.py
+++ facial_landmarks_white_bg.py
@@ -5,37 +5,36 @@ import numpy as np
 cap = cv2.VideoCapture()
 cap.open(0, cv2.CAP_DSHOW)
 
-
-
 # Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh()
 
-
-
 while True:
     # Image
-    ret, image = cap.read()
+    ret, img = cap.read()
     if ret is not True:
         break
-    height, width, _ = image.shape
-    black_frame = np.zeros((height, width, 3), np.uint8)
-    black_frame.fill(255)
-    #print("Height, width", height, width)
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    height, width, _ = img.shape
+    white_frame = np.zeros((height, width, 3), np.uint8)
+    white_frame.fill(255)
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # Facial landmarks
-    result = face_mesh.process(rgb_image)
-
-    for facial_landmarks in result.multi_face_landmarks:
-        for i in range(0, 468):
-            pt1 = facial_landmarks.landmark[i]
-            x = int(pt1.x * width)
-            y = int(pt1.y * height)
-
-            cv2.circle(black_frame, (x, y), 2, (100, 100, 0), -1)
-            #cv2.putText(image, str(i), (x, y), 0, 1, (0, 0, 0))
-
-    cv2.imshow("Image", image)
-    cv2.imshow("Black", black_frame)
-    cv2.waitKey(1)
+    result = face_mesh.process(rgb_img)
+    try:
+        for facial_landmarks in result.multi_face_landmarks:
+            for i in range(0, 468):
+                pt1 = facial_landmarks.landmark[i]
+                x = int(pt1.x * width)
+                y = int(pt1.y * height)
+                cv2.circle(white_frame, (x, y), 2, (100, 100, 0), -1)
+    except Exception as e:
+        pass
+    cv2.imshow("Image", img)
+    cv2.imshow("Black", white_frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+        
+cv2.destroyAllWindows()        
+cap.release()
